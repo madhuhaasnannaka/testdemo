882018f30f7b966b5805bb1aa343d377df95e37b
MADHUHAAS NANNAKA <madhuhaasnannaka@gmail.com>
2020-02-03 06:04:18 +0000

    sonar-project.js created online with Bitbucket

---
+++ sonar-project.js
@@ -0,0 +1,22 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+     sonarqubeScanner({
+       serverUrl: 'https://sonarcloud.io/',
+       token : '4ea57303c832f76242362c6cd9c9a73e63c27016',
+       options : {
+       'sonar.login': '4ea57303c832f76242362c6cd9c9a73e63c27016',
+       'sonar.organization': 'lttsdevops',
+       'sonar.projectKey': 'lttsdevops_sample-nodejs-project',
+       'sonar.projectVersion': '1.0',
+       'sonar.language': 'js',
+       'sonar.sourceEncoding': 'UTF-8',
+       //'sonar.sources': '.',
+       'sonar.inclusions' : 'src/**', // Entry point of your code
+       'sonar.exclusions': '**/*.test.*',
+       'sonar.tests': '.',
+       'sonar.test.inclusions': '**/testing/**,**/*.spec.ts',
+       //'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+       //'sonar.testExecutionReportPaths': 'test-report.xml'
+       
+
+       }
+     }, () => {});
