0bdd669f83c6893be1f6206b9f3dc590d00a8289
dileepbodapati92 <59645350+dileepbodapati92@users.noreply.github.com>
2020-03-13 12:37:48 +0530

    latest changed

--- sonar-project.js
+++ sonar-project.js
@@ -1,11 +1,11 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
        serverUrl: 'https://sonarcloud.io/',
-       token : '4ea57303c832f76242362c6cd9c9a73e63c27016',
+       token : '426db0aaf54106db935b090f8be8e915086b1f7f',
        options : {
-       'sonar.login': '4ea57303c832f76242362c6cd9c9a73e63c27016',
-       'sonar.organization': 'lttsdevops',
-       'sonar.projectKey': 'lttsdevops_sample-nodejs-project',
+       'sonar.login': '426db0aaf54106db935b090f8be8e915086b1f7f',
+       'sonar.organization': 'dileepbodapati92',
+       'sonar.projectKey': ' dileepbodapati92',
        'sonar.projectVersion': '1.0',
        'sonar.language': 'js',
        'sonar.sourceEncoding': 'UTF-8',
